ba2986e5f7d26fbed8bfe0d273dc73fad56b9dd4
CGM123sherry <g.christinemurithi@gmail.com>
2024-07-22 13:04:30 +0300

    completed the array map method lab

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
-const titleCased = () => {
-  return tutorials
+const specialWords = {
+  OO: "OO",
+  API: "API",
+  NaN: "NaN",
+  JSONP: "JSONP",
+  stopPropagation: "stopPropagation",
+  preventDefault: "preventDefault",
+};
+function capitalizedWord(word) {
+  const lowerCaseWord = word.toLowerCase();
+  if (specialWords.hasOwnProperty(lowerCaseWord)) {
+    return specialWords[lowerCaseWord];
+  }
+  return word.replace(/\b\w/g, (char) => char.toUpperCase());
 }
+const titleCased = () => {
+  return tutorials.map((tutorial) =>
+    tutorial.split(" ").map(capitalizedWord).join(" ")
+  );
+};
+console.log(titleCased());
